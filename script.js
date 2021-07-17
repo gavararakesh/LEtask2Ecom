eafa3283252cd25adcefc2b1aaca0c9ecdf69e03
gavararakesh <63574324+gavararakesh@users.noreply.github.com>
2021-07-17 21:05:53 +0530

    scripts done

---
+++ script.js
@@ -0,0 +1,81 @@
+function item1(){
+	if (document.getElementById('item1').style.display==="none"){
+		document.getElementById('item1').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item1').style.display="none";
+		dis1=false;
+	}
+}
+function item2(){
+	if (document.getElementById('item2').style.display==="none"){
+		document.getElementById('item2').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item2').style.display="none";
+		dis1=false;
+	}
+}
+function item3(){
+	if (document.getElementById('item3').style.display==="none"){
+		document.getElementById('item3').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item3').style.display="none";
+		dis1=false;
+	}
+}
+function item4(){
+	if (document.getElementById('item4').style.display==="none"){
+		document.getElementById('item4').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item4').style.display="none";
+		dis1=false;
+	}
+}
+function item5(){
+	if (document.getElementById('item5').style.display==="none"){
+		document.getElementById('item5').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item5').style.display="none";
+		dis1=false;
+	}
+}
+function item6(){
+	if (document.getElementById('item6').style.display==="none"){
+		document.getElementById('item6').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item6').style.display="none";
+		dis1=false;
+	}
+}
+function item7(){
+	if (document.getElementById('item7').style.display==="none"){
+		document.getElementById('item7').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item7').style.display="none";
+		dis1=false;
+	}
+}
+function item8(){
+	if (document.getElementById('item8').style.display==="none"){
+		document.getElementById('item8').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item8').style.display="none";
+		dis1=false;
+	}
+}
+function item9(){
+	if (document.getElementById('item9').style.display==="none"){
+		document.getElementById('item9').style.display="block";
+		dis1=true;
+	}else{
+		document.getElementById('item9').style.display="none";
+		dis1=false;
+	}
+}
